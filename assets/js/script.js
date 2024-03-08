56bdcb245364e2e33c085ed77b874e8899239d43
Doug Scheible <doug_scheible@yahoo.com>
2024-03-07 18:41:50 -0600

    Updated Tailwind options started adding sports API info to blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
-
 var dateEl = document.querySelector('.date');
-var currentWeatherEl = document.querySelector('.current-weather');
+var sportsEl = document.querySelector('.sports');
 
 var dateFormats = ["dddd, MMM D, YYYY", "MMM D, YYYY", "MMMM D, YYYY h:mm A", "ddd, MMM D, YYYY h:mm A" ];
+var sportsTeams = ["Spurs", "Mavericks"];
+
 var currentDateFormatIndex = 0;
 
-dateEl.innerHTML = dayjs().format(dateFormats[currentDateFormatIndex]);
+sportsAPI_key = "b5074573df57c56932b0b3096843718d";
 
-
+console.log("SCRIPT LOADING");
 
 const nextDateFormat = function() {
     console.log("DATE CLICKED");
@@ -20,23 +21,76 @@ const nextDateFormat = function() {
     }
 }
 
-const buildCurrentWeather = function () {
-    console.log("TODAY:" + dayjs().format(dateFormats[0]));
+const buildSports = function (teams) {
 
-    var weatherEl = document.createElement("ul");
-    var weatherTodayEl = document.createElement("li");
+    let sportsAPI_URL ="https://v2.nba.api-sports.io/standings?league=standard&season=2023&team=31";
+    let teamLogoURL = "";
+    let winTotal = 0;
+    let lossTotal = 0;
 
-    weatherTodayEl.textContent = dayjs().format(dateFormats[0]);
-    console.log(dayjs());
-    console.log(weatherTodayEl);
+    var myHeaders = new Headers();
+    myHeaders.append("x-rapidapi-key", sportsAPI_key);
+    myHeaders.append("x-rapidapi-host", "v2.nba.api-sports.io");
 
-    weatherEl.appendChild(weatherTodayEl);
-    console.log("HERE");
-    currentWeatherEl.appendChild(weatherEl);
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    /*fetch(sportsAPI_URL, requestOptions)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+
+                    winTotal = data['response'][0]['win']['total'];
+                    lossTotal = data['response'][0]['loss']['total'];
+                    teamLogoURL = data['response'][0]['team']['logo'];
+
+                    console.log( winTotal, lossTotal, teamLogoURL);
+
+                    let teamInfoEl = document.createElement("ul");
+                    let teamLogoEl = document.createElement("img");
+
+                    teamLogoEl.setAttribute("src", teamLogoURL);
+                    teamLogoEl.setAttribute("class", "teamIcon");
+
+                    teamInfoEl.appendChild(teamLogoEl);
+
+                    currentSportsEl.appendChild(teamInfoEl);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })*/
+    /*let teamInfoEl = document.createElement("div");*/
+
+    let sportsContainerEl = document.createElement("div");
+    sportsContainerEl.setAttribute("class", "flex");
+
+    let teamIconEl = document.createElement("img");
+    teamIconEl.setAttribute("src", "assets/pics/logo.png");
+    teamIconEl.setAttribute("class", "teamIcon flex-row w-1/5 p-3" );
+    
+    let winsLossesEl = document.createElement("div");
+    winsLossesEl.setAttribute("class", "flex items-center");
+    winsLossesEl.innerHTML = "WINS AND LOSSES GO HERE";
+
+    sportsContainerEl.appendChild(teamIconEl);
+    sportsContainerEl.appendChild(winsLossesEl);
+
+    sportsEl.appendChild(sportsContainerEl);
 
 }
 
-dateEl.addEventListener('click', nextDateFormat);
 
-//console.log(dayjs());
-buildCurrentWeather();
+currentDate = dayjs().format(dateFormats[currentDateFormatIndex]);
+dateEl.innerHTML = dayjs().format(dateFormats[currentDateFormatIndex]);
+
+buildSports(sportsTeams);
+
+console.log("HERE DATE:" + currentDate);
+
+
+dateEl.addEventListener('click', nextDateFormat);
